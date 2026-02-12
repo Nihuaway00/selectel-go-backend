9c6eb12fc2d56c245b1004b19081e5944c48c4cf
Nikita <troshin.nksr@gmail.com>
2026-02-12 16:44:32 +0300

    Поправил examples

--- examples/sample.go
+++ examples/sample.go
@@ -3,10 +3,11 @@ package main
 import "log/slog"
 
 func main() {
-	slog.Info("Starting server")             // should start with lowercase
+	slog.Info("Starting server")             // should start with lowercase, will replace «S» with «s»
 	slog.Info("запуск сервера")              // non-English letters
 	slog.Info("server started!")             // special chars
 	slog.Info("user password: 123")          // sensitive data
 	slog.Info("user secret=123")             // sensitive data
-	slog.Info("Server started successfully") // ok
+	slog.Info("server started successfully") // ok
+	slog.Info("Server started successfully") // should start with lowercase, will replace «S» with «s»
 }
